9d3dc68c4fb229a3fd70750119da655f7bf2c499
Vandhuy Martins <vandhuy@gmail.com>
2018-01-09 02:41:20 -0300

    Improving items.xml

--- src/main/java/com/tibia/helper/ConstantsHelper.java
+++ src/main/java/com/tibia/helper/ConstantsHelper.java
@@ -4,7 +4,7 @@ public class ConstantsHelper {
 	/**
 	 * Dell
 	 */
-	public final int GOLD_PER_ITEM = 30000;
+	/*public final int GOLD_PER_ITEM = 30000;
 	
 	public final int CLOSE_MARKET_X = 1279;
 	public final int CLOSE_MARKET_Y = 699;
@@ -41,47 +41,47 @@ public class ConstantsHelper {
 	public final int FIRST_SELLER_END_AT_X_TOP = 1130;
 	public final int FIRST_SELLER_END_AT_Y_TOP = 447;
 	public final int FIRST_SELLER_END_AT_X_BOTTOM = 1302;
-	public final int FIRST_SELLER_END_AT_Y_BOTTOM = 461;
+	public final int FIRST_SELLER_END_AT_Y_BOTTOM = 461;*/
 	
 	/**
 	 * MacBook
 	 */
-//	public final int GOLD_PER_ITEM = 30000;
-//	
-//	public final int CLOSE_MARKET_X = 950;
-//	public final int CLOSE_MARKET_Y = 635;
-//	
-//	public final int SEARCH_BOX_X = 330;
-//	public final int SEARCH_BOX_Y = 595;
-//	
-//	public final int FIRST_FOUND_X = 300;
-//	public final int FIRST_FOUND_Y = 300;
-//	
-//	public final int FIRST_SELLER_X = 680;
-//	public final int FIRST_SELLER_Y = 390;
-//	
-//	public final int BUY_RADIO_X = 455;
-//	public final int BUY_RADIO_Y = 550;
-//	
-//	public final int ITEM_QUANTITY_X = 778;
-//	public final int ITEM_QUANTITY_Y = 549;
-//	
-//	public final int PIECE_PRICE_X = 650;
-//	public final int PIECE_PRICE_Y = 570;
-//	
-//	public final int ANONYMOUS_CHECKBOX_X = 865;
-//	public final int ANONYMOUS_CHECKBOX_Y = 595;
-//	
-//	public final int CREATE_OFFER_X = 975;
-//	public final int CREATE_OFFER_Y = 595;
-//	
-//	public final int PIECE_PRICE_X_TOP = 642;
-//	public final int PIECE_PRICE_Y_TOP = 383;
-//	public final int PIECE_PRICE_X_BOTTOM = 724;
-//	public final int PIECE_PRICE_Y_BOTTOM = 397;
-//	
-//	public final int FIRST_SELLER_END_AT_X_TOP = 812;
-//	public final int FIRST_SELLER_END_AT_Y_TOP = 383;
-//	public final int FIRST_SELLER_END_AT_X_BOTTOM = 983;
-//	public final int FIRST_SELLER_END_AT_Y_BOTTOM = 397;
+	public final int GOLD_PER_ITEM = 30000;
+	
+	public final int CLOSE_MARKET_X = 950;
+	public final int CLOSE_MARKET_Y = 635;
+	
+	public final int SEARCH_BOX_X = 330;
+	public final int SEARCH_BOX_Y = 595;
+	
+	public final int FIRST_FOUND_X = 300;
+	public final int FIRST_FOUND_Y = 300;
+	
+	public final int FIRST_SELLER_X = 680;
+	public final int FIRST_SELLER_Y = 390;
+	
+	public final int BUY_RADIO_X = 455;
+	public final int BUY_RADIO_Y = 550;
+	
+	public final int ITEM_QUANTITY_X = 778;
+	public final int ITEM_QUANTITY_Y = 549;
+	
+	public final int PIECE_PRICE_X = 650;
+	public final int PIECE_PRICE_Y = 570;
+	
+	public final int ANONYMOUS_CHECKBOX_X = 865;
+	public final int ANONYMOUS_CHECKBOX_Y = 595;
+	
+	public final int CREATE_OFFER_X = 975;
+	public final int CREATE_OFFER_Y = 595;
+	
+	public final int PIECE_PRICE_X_TOP = 642;
+	public final int PIECE_PRICE_Y_TOP = 383;
+	public final int PIECE_PRICE_X_BOTTOM = 724;
+	public final int PIECE_PRICE_Y_BOTTOM = 397;
+	
+	public final int FIRST_SELLER_END_AT_X_TOP = 812;
+	public final int FIRST_SELLER_END_AT_Y_TOP = 383;
+	public final int FIRST_SELLER_END_AT_X_BOTTOM = 983;
+	public final int FIRST_SELLER_END_AT_Y_BOTTOM = 397;
 }
