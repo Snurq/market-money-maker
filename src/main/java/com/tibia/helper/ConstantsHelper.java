057453a31e349543b3778c20b19d83b87a8463ba
Vandhuy Martins <vandhuy@gmail.com>
2018-01-17 21:58:09 -0300

    Updating items.xml

--- src/main/java/com/tibia/helper/ConstantsHelper.java
+++ src/main/java/com/tibia/helper/ConstantsHelper.java
@@ -1,8 +1,8 @@
 package com.tibia.helper;
 
 public class ConstantsHelper {
-  	//public final String CHARACTER_NAME = "Gordo Dhuy";
-	public final String CHARACTER_NAME = "Warlock Dhuy";
+  	public final String CHARACTER_NAME = "Gordo Dhuy";
+	//public final String CHARACTER_NAME = "Warlock Dhuy";
 	
 	public final String PROGRAM_TITLE = "MarketMaker 1.0 by Dhuy";
 	public final int MAX_ACCEPTED_LATENCY = 500;
